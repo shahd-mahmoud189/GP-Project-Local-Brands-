e8bf69a05a45cf8aff79b3837d9022cafccb65d7
shahd-mahmoud189 <shmahmoud189@gmail.com>
2026-04-23 20:04:46 +0200

    brandownwer

--- src/app/_components/sharedComponents/Nav/Nav.tsx
+++ src/app/_components/sharedComponents/Nav/Nav.tsx
@@ -15,7 +15,7 @@ export default function Nav() {
 
   const getAccountLink = (role: string) => {
     if (role === "admin") return "/adminAccount";
-    if (role === "owner") return "/ownerAccount";
+    if (role === "BrandOwner") return "/ownerAccount";
     return "/customerAccount";
   };
   
